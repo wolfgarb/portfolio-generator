ca42710d675c78d6f988d918d40b8127b4f0fc9c
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-10-20 10:22:01 -0400

    Add first question

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const profDataArgs = process.argv.slice(2);
-const [name, github] = profDataArgs;
+const inquirer = require('inquirer')
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw new Error(err);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-    console.log('Portfolio complete!');
-})
+// const pageHTML = generatePage(name, github)
+
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log('Portfolio complete!');
+// })
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+]).then(answers => console.log(answers));
