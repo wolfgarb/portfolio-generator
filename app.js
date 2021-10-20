a3431afab75186ab8d8be15946b77d031b7ab9cc
wolfgarb <88980251+wolfgarb@users.noreply.github.com>
2021-10-20 10:42:25 -0400

    Question recursion

--- app.js
+++ app.js
@@ -10,11 +10,84 @@ const inquirer = require('inquirer')
 
 //     console.log('Portfolio complete!');
 // })
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+          },
+          {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+          }
+    ])
+};
 
-inquirer.prompt([
-    {
+const promptProject = portfolioData => {
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `)
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    return inquirer.prompt([
+      {
         type: 'input',
         name: 'name',
-        message: 'What is your name?'
-    }
-]).then(answers => console.log(answers));
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+  };
+
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    })
+    .then(answers => console.log(answers))
+    .then(projectAnswers => console.log(projectAnswers));
